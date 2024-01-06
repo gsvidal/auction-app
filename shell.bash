4736057903dce0a5fab98c314413ce96dc1795dd
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2024-01-05 23:55:11 -0500

    Add css type

---
+++ shell.bash
@@ -0,0 +1,29 @@
+from auctions.models import AuctionListing, User, Category
+
+art = Category(name="Art")
+art.save()
+
+electronics = Category(name="Electronics")
+electronics.save()
+
+fashion = Category(name="Fashion")
+fashion.save()
+
+home = Category(name="Home")
+home.save()
+
+vehicles = Category(name="Vehicles")
+vehicles.save()
+
+
+# Postgres
+
+INSERT INTO auctions_category (name) VALUES
+('Electronics'),
+('Fashion'),
+('Grocery'),
+('Books'),
+('Music'),
+('Sports'),
+('Games'),
+('No Category');
