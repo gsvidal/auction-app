a003ad7b170efec9d087f3d430d5c87394139fcb
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2024-01-06 00:53:05 -0500

    setup static files

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -9,13 +11,23 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("create_listing", views.create_listing, name="create_listing"),
     path("listings/<int:listing_id>", views.listings, name="listings"),
-    path('add_to_watchlist/<int:listing_id>/', views.add_to_watchlist, name='add_to_watchlist'),
-    path('remove_from_watchlist/<int:listing_id>/', views.remove_from_watchlist, name='remove_from_watchlist'),
-    path('watchlist', views.watchlist, name='watchlist'),
+    path(
+        "add_to_watchlist/<int:listing_id>/",
+        views.add_to_watchlist,
+        name="add_to_watchlist",
+    ),
+    path(
+        "remove_from_watchlist/<int:listing_id>/",
+        views.remove_from_watchlist,
+        name="remove_from_watchlist",
+    ),
+    path("watchlist", views.watchlist, name="watchlist"),
     path("categories", views.categories, name="categories"),
     path("by_categories/<int:category_id>", views.by_categories, name="by_categories"),
     path("seller/<int:seller_id>", views.seller, name="seller"),
     path("bid/<int:listing_id>", views.bid, name="bid"),
     path("close_listing/<int:listing_id>", views.close_listing, name="close_listing"),
-    path("delete_listing/<int:listing_id>", views.delete_listing, name="delete_listing")
-]
+    path(
+        "delete_listing/<int:listing_id>", views.delete_listing, name="delete_listing"
+    ),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- commerce/settings.py
+++ commerce/settings.py
@@ -29,9 +29,9 @@ DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -54,75 +54,75 @@ DATABASES["default"] = dj_database_url.parse(database_url)
 # database_url = "postgres://auction_db_768g_user:ATvdAGDzJDQXoenvEA11h18pmKOAzniQ@dpg-cmc9upgl5elc739f17a0-a.oregon-postgres.render.com/auction_db_768g"
 # DATABASES["default"] = dj_database_url.parse(database_url)
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'auctions',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "auctions",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MESSAGE_TAGS = {
-    'comment_error': 'comment_error',  # Define your custom tag here
+    "comment_error": "comment_error",  # Define your custom tag here
 }
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # Here
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'commerce.urls'
+ROOT_URLCONF = "commerce.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'commerce.wsgi.application'
+WSGI_APPLICATION = "commerce.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-AUTH_USER_MODEL = 'auctions.User'
+AUTH_USER_MODEL = "auctions.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -130,9 +130,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'US/Eastern'
+TIME_ZONE = "US/Eastern"
 
 USE_I18N = True
 
@@ -144,10 +144,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    # Additional locations of static files
-    os.path.join(BASE_DIR, "static"),
-]
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATIC_URL = "/static/"
+
+# Following settings only make sense on production and may break development environments.
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+# https://help.pythonanywhere.com/pages/DjangoStaticFiles/
